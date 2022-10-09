e38d29e0af903a6cf86d4803237cb84c6a109f42
nhan <letranhuunhan@gmail.com>
2022-10-09 16:19:16 +0700

    build fakeapi app

--- terraform/module/vm/input.tf
+++ terraform/module/vm/input.tf
@@ -1,38 +1,38 @@
 
 
-# Resource Group/Location
-variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
-  default = "East US"
-}
+# # Resource Group/Location
+# variable "location" {
+#   description = "The Azure Region in which all resources in this example should be created."
+#   default = "East US"
+# }
 
-variable "resource_group" {
-  description = "Name of resource"
-  default = "Azuredevops"
-}
+# variable "resource_group" {
+#   description = "Name of resource"
+#   default = "Azuredevops"
+# }
 
-variable "application_type" {
-  description = "Application type"
-  default = "Webapp1"
-}
+# variable "application_type" {
+#   description = "Application type"
+#   default = "Webapp1"
+# }
 
-# Network
-#variable virtual_network_name {}
-variable "address_prefix_test" {
-  description = "Adress space"
-  default = "10.0.2.0/24"
-}
+# # Network
+# #variable virtual_network_name {}
+# variable "address_prefix_test" {
+#   description = "Adress space"
+#   default = "10.0.2.0/24"
+# }
 
-variable address_space {
-  description = "Adress space"
-  default = "10.0.0.0/22"
-}
+# variable address_space {
+#   description = "Adress space"
+#   default = "10.0.0.0/22"
+# }
 
-variable "resource_type" {
-  description = "Resource type"
-  default = "batchAccount"
-}
+# variable "resource_type" {
+#   description = "Resource type"
+#   default = "batchAccount"
+# }
 
-variable "admin_username" {}
-variable "public_ip_address" {}
-variable "subnet_id" {}
+# variable "admin_username" {}
+# variable "public_ip_address" {}
+# variable "subnet_id" {}
