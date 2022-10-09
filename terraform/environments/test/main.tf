0ee6cfa5c82e79c9f4b3b3a221ca58eddc3faa8b
nhan <letranhuunhan@gmail.com>
2022-10-09 13:30:24 +0700

    deploy infratructure

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -52,3 +52,15 @@ module "publicip" {
   resource_type    = "publicip"
   resource_group   = "${var.resource_group}"
 }
+
+module "vmlinux" {
+  source            = "./modules/vm"
+  location          = "${var.location}"
+  application_type  = "${var.application_type}"
+  resource_type     = "VMLinux"
+  resource_group    = var.resource_group_name
+  admin_username    = "agent"
+  subnet_id         = "${module.network.subnet_id_test}"
+  public_ip_address = "${module.publicip.public_ip_address_id}"
+  
+}
